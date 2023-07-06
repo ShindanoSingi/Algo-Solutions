eaa7e0905fcc52b971180189d8145582d1e612c7
Shindano Singi <101299687+ShindanoSingi@users.noreply.github.com>
2023-07-05 21:52:00 -0500

    Time: 60 ms (75.14%) | Memory: 44.7 MB (32.83%) - LeetSync

---
+++ chunk-array/chunk-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    let newArr = [];
+    let index = 0;
+
+    while(index < arr.length){
+        newArr.push(arr.slice(index, index + size))
+        index += size;
+    }
+    return newArr;
+};
